221182a3d0fe0024838c0cae8646939d66cafdd1
lorecab12 <abogadalorenacabrera@gmail.com>
2022-04-21 23:15:54 -0300

    modifique resta

--- nuevo.js
+++ nuevo.js
@@ -2,3 +2,9 @@ function suma(numeroA, numeroB){
     return numeroA + numeroB
 }
 console.log(suma(5, 9));
+
+
+function resta(numeroA, numeroB){
+    return numeroA - numeroB
+}
+console.log(resta(5, 9));
